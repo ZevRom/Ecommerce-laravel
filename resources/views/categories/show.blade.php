ae5eac4d7f858d19501c9313895b22f0fbcf1a48
ZevRom <sorsrz@gmail.com>
2023-05-30 20:43:40 -0500

    Detalle de Categorías, la vista

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,5 +1,12 @@
 <x-app-layout>
 
-    {{$category->name}}
+    <div class="containerx py-8">
+        <figure class="mb-4">
+            <img class="w-full h-80 object-cover object-center" src="{{ Storage::url($category->image) }}" alt="">
+        </figure>
+
+        @livewire('category-filter',['category' => $category])
+
+    </div>
 
 </x-app-layout>
